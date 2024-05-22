571406ca9b1ed819d78610c4349c30c4f711cee4
nick <nickj8366@gmail.com>
2024-05-22 13:02:21 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
-const { Telegraf, Markup } = require("telegraf");
+const { Telegraf } = require("telegraf");
 
 // Use environment variable for the token
 const Token = "7077712875:AAGTunvFL7vuYHAlsqSg0tD6YvnvlFDZk_M";
 
 const bot = new Telegraf(Token);
 
-const web_url = "https://bot-game-eight.vercel.app/";
+const web_url = "https://bot-game-eight.vercel.app";
 
 bot.start((ctx) => {
   const username = ctx.from.username
@@ -13,10 +13,9 @@ bot.start((ctx) => {
     : ctx.from.first_name;
   ctx.reply(`Welcome ${username}`, {
     reply_markup: {
-      keyboard: [[{ text: "Launch App" }]],
+      keyboard: [[{ text: "Launch App", web_app: { url: web_url } }]],
       resize_keyboard: true, // Optional: Automatically resize the keyboard to fit the buttons
       one_time_keyboard: true, // Optional: Hide the keyboard after a button is pressed
-      inline_keyboard: [[{ text: "Launch App", url: web_url }]] // Inline button
     },
   });
 });
